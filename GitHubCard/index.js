6c75d5b8bfc4b481f2a4af9fa2c7b89cd75a64eb
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-03 20:27:56 -0400

    Stretch Goal #1 and notes

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,16 +4,39 @@
 */
 
 axios
-  .get(" https://api.github.com/users/chelsabeth")
+.get("https://api.github.com/users/chelsabeth/followers")
+.then(response => {
+  console.log(response.data)
+  parseUser('https://api.github.com/users/chelsabeth') // Makes a card for me :)
+  response.data.forEach(element => { // Loops through my followers list
+    parseUser(element['url']); // Passes in each of the urls from my followers list
+  });
+});
+
+function parseUser(user) {
+  axios
+  .get(`${user}`) // Pass In the users URL for their GitHub Profile
   .then(response => {
-    console.log("Github User Data", response.data);
-    const card = userCard(response.data); // HTML Div Element
     const cards = document.querySelector(".cards"); // cards is now equal to cards div in HTML
-    cards.appendChild(card);
+    console.log("Github User Data", response.data);
+    cards.appendChild(userCard(response.data)); // Makes the card that is returned from the userCard function a child of the cards div in HTML
   })
   .catch(error => {
     console.log("Sorry, an error has occured", error);
   });
+  // return userCard(response.data); // HTML Div Element
+}
+
+  // .get(" https://api.github.com/users/chelsabeth")
+  // .then(response => {
+  //   console.log("Github User Data", response.data);
+  //   const card = userCard(response.data); // HTML Div Element
+  //   const cards = document.querySelector(".cards"); // cards is now equal to cards div in HTML
+  //   cards.appendChild(card);
+  // })
+  // .catch(error => {
+  //   console.log("Sorry, an error has occured", error);
+  // });
   
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -37,7 +60,7 @@ axios
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['BCtopics'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
